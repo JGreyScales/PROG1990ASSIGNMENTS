30d724b6dde3238021d8d143beade417ab8f6b26
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2024-10-10 04:16:28 -0400

    typo fix

--- Assignment 3/q2/Utils.c
+++ Assignment 3/q2/Utils.c
@@ -1,6 +1,6 @@
 #include "Utils.h"
 
-// Implimentation of Utils
+// Implementation of Utils
 // Jackson G - Prog71990 - Assignment 3 Question 2 - 2024-10-06
 
 double sum_of_smallest_and_largest(double doubleArray[], int arrLength) {
